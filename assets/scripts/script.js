02c0af2b2fb574aadcac82ad46839ea1fcd12d1f
DLittlefield81 <dennis.ah.littlefield@gmail.com>
2022-05-19 07:15:53 -0400

    line 40 issue

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,41 +6,46 @@
 // OUTPUT a password is generated that matches the selected criteria
 // password is either displayed in an alert or written to the page
 
+let charUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let charLower = "abcdefghijklmnopqrstuvwxyz";
+let charSpecial = "!@#$%^&*()`~-_=+[]{}:;'<,>./?";
+let charNumber = "0123456789";
 
 
 
-
-
-let random = Math.floor(Math.random() * pcChoice.length);
-console.log(pcChoice[random]);
-for (let i = 0; i < passwordLength; i++) {}
 
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  let passwordLength = window.prompt("How many characters do you want in your password?"); //Input popup passwordLength
-  let askSmalCase = window.confirm("Would you like to use small letters?"); // Boolean popup
-  let askAlphaCap = window.confirm("Would you like to use capitol letters?"); // Boolean popup
+  let askLength = window.prompt("How many characters do you want in your password?"); //Input popup passwordLength
+  let askLower = window.confirm("Would you like to use small letters?"); // Boolean popup
+  let askUpper = window.confirm("Would you like to use capitol letters?"); // Boolean popup
   let askSpecial = window.confirm("Would you like to use special characters?"); //Boolean popup
   let askNumber = window.confirm("would you like to add numbers?"); //Boolean popup
 
 
-  let password = generatePassword(passwordLength,charSmall,charCaps,charSpecial,charNumber); //values from inputs
-  let passwordText = document.querySelector("#password");
-  l;
-  passwordText.value = password;
+  let password = generatePassword(askLength,charLower,charUpper,charSpecial,charNumber); //values from inputs
+  let passwordText = document.querySelector("#password");//finds password box in document
+  passwordText.value = password;//Output Password to form
 }
-function generatePassword(passwordLength,charSmall,charCaps,charSpecial,charNumber){ 
-  let charCaps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let charSmall = "abcdefghijklmnopqrstuvwxyz";
-let charSpecial = "!@#$%^&*()";
-let charNumber = "0123456789";
+function generatePassword(askLength,charLower,charUpper,charSpecial,charNumber){ 
+let passString = charSmall
+if (askUpper) passString = passString.concat(charUpper);
+if (askSpecial) passString = passString.concat(charSpecial);
+if (askNumber) passString = passString.concat(charNumber);
+let passwordCharacters = ""; //Store characters
+// let myArray =passwordCharacters.split("")
+for (let x = 0, x < askLength x++ ){
+  let character =passwordCharacters[Math.floor(Math.random()*askLength)];
+passwordCharacters.push(character)
+}
+console.log(charSmall)
 }
 
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-return pass;
+// return pass;
