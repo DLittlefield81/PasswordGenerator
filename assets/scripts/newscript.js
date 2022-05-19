96b4a611b2fe5fa318f510a873653a39abfc514f
DLittlefield81 <dennis.ah.littlefield@gmail.com>
2022-05-19 17:46:07 -0400

    completed working on project, readme to complete

--- assets/scripts/newscript.js
+++ assets/scripts/newscript.js
@@ -1,8 +1,14 @@
-var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var passwordLength = 12;
-var password = "";
+var password=document.getElementById("generate");
 
-for (var i = 0; i <= passwordLength; i++) {
-    var randomNumber = Math.floor(Math.random() * chars.length);
-    password += chars.substring(randomNumber, randomNumber +1);
-   }
+ function writePassword() {
+    var chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var passwordLength = 12;
+    var password = "";
+ for (var i = 0; i <= passwordLength; i++) {
+   var randomNumber = Math.floor(Math.random() * chars.length);
+   password += chars.substring(randomNumber, randomNumber +1);
+   console.log(password)
+  }
+        document.getElementById("password").value = password;
+ }
+ generateBtn.addEventListener("click", writePassword);
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,6 +5,10 @@
 // OUTPUT a password is generated that matches the selected criteria
 // password is either displayed in an alert or written to the page
 
+let askLower
+let askUpper
+let askSpecial
+let askNumber
 let charUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let charLower = "abcdefghijklmnopqrstuvwxyz";
 let charSpecial = "!@#$%^&*()`~-_=+[]{}:;'<,>./?";
@@ -12,60 +16,71 @@ let charNumber = "0123456789";
 let askLength = 8;
 
 // Assignment Code
-let generateBtn = document.querySelector("#generate");//finds generate button to run code
+let generateBtn = document.querySelector("#generate"); //finds generate button to run code
 
 // Write password to the #password input
 function writePassword() {
+
   askLength = window.prompt(
-    " How many characters would you like in your password? (8-128)",8); //Input popup passwordLength
-  console.log(askLength);
+    " How many characters would you like in your password? (8-128)",
+    8
+  ); //Input popup passwordLength
+
+  //USER INPUT LENGTH
+ 
+  //========
   if (askLength < 8 || askLength > 128) {
     writePassword(); //restart if password length does not meet 8-128 rule
-  } else{ //continue run
-    let askLower = window.confirm("Would you like to use small letters?"); // Boolean popup
-  let askUpper = window.confirm("Would you like to use capitol letters?"); // Boolean popup
-  let askSpecial = window.confirm("Would you like to use special characters?"); //Boolean popup
-  let askNumber = window.confirm("would you like to add numbers?"); //Boolean popup
+  } else {
+    //continue run
+    
+    askLower = window.confirm("Would you like to use small letters?"); // Boolean popup
+    askUpper = window.confirm("Would you like to use capitol letters?"); // Boolean popup
+    if (!askLower && !askUpper) askLower = true;
+    console.log(askLower + ' ' + askUpper)
+    askSpecial = window.confirm(
+      "Would you like to use special characters?"
+    ); //Boolean popup
+    askNumber = window.confirm("would you like to add numbers?"); //Boolean popup
   }
-//=============
-// create string table
-//run math
+  //=============
+  // create string table
+  //run math
 
-
-
-
-
-//=============
+  //=============
   let passwordText = document.querySelector("#password"); //finds password box in document
-  passwordText.value = password; //Output Password to form
+  passwordText.value = generatePassword(askLength, askLower, askUpper, askSpecial, askNumber); //Output Password to form
 }
-let password = generatePassword(askLength,charLower, charUpper, charSpecial,charNumber);{ //values from inputs
+
 function generatePassword(
   askLength,
-  charLower,
-  charUpper,
-  charSpecial,
-  charNumber
+  askLower,
+  askUpper,
+  askSpecial,
+  askNumber
 ) {
+
+  //values from inputs
+  let password = "";
   let passString = "";
-  if (askLower) passString = passString.concat(charLower);
-  if (askUpper) passString = passString.concat(charUpper);
-  if (askSpecial) passString = passString.concat(charSpecial);
-  if (askNumber) passString = passString.concat(charNumber);
-  let chars = ""; //Store characters
-
-  // let myArray =passwordCharacters.split("")
-  for (let x = 0; x <= askLength; x++) {
-    let characterCode =
-      randomNumber[Math.floor(Math.random() * chars.length)]; //random character from length of available letters
-    password += chars.substring(randomNumber, randomNumber +1);
-  console.log(password);
-}
+  let verify =0;
+  if (askLower) {passString = passString.concat(charLower); verify++; password += charLower[Math.floor(Math.random() * charLower.length)];}; 
+  if (askUpper){ passString = passString.concat(charUpper); verify++; password += charUpper[Math.floor(Math.random() * charUpper.length)];}
+  if (askSpecial) {passString = passString.concat(charSpecial); verify++; password += charSpecial[Math.floor(Math.random() * charSpecial.length)];}
+  if (askNumber) {passString = passString.concat(charNumber); verify++; password += charNumber[Math.floor(Math.random() * charNumber.length)];}
+   //Store characters
+  //========console full string
   
-  
+
+
+
+  for (let x = verify; x < askLength; x++) {
+    let characterCode = passString[Math.floor(Math.random() * passString.length)]; //random character from length of available letters
+    password += characterCode;
+ 
+  }
+
+return password;
 }
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);//button that begins running writePassword function
-
-// return pass;
+// Add event listener to run function
+generateBtn.addEventListener("click", writePassword); //button that begins running writePassword function
