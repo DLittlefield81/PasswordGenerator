939fb1209c2e71ceb2f3d8efffd25f78d0ed16ee
DLittlefield81 <dennis.ah.littlefield@gmail.com>
2022-05-19 08:56:17 -0400

    added default value to prompt, added loop for prompt length if changed

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -10,7 +10,7 @@ let charUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let charLower = "abcdefghijklmnopqrstuvwxyz";
 let charSpecial = "!@#$%^&*()`~-_=+[]{}:;'<,>./?";
 let charNumber = "0123456789";
-
+let askLength = 8
 
 
 
@@ -19,29 +19,38 @@ let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  let askLength = window.prompt("How many characters do you want in your password?"); //Input popup passwordLength
+  askLength = window.prompt(" How many characters would you like in your password? (8-128)",8); //Input popup passwordLength
   let askLower = window.confirm("Would you like to use small letters?"); // Boolean popup
   let askUpper = window.confirm("Would you like to use capitol letters?"); // Boolean popup
   let askSpecial = window.confirm("Would you like to use special characters?"); //Boolean popup
   let askNumber = window.confirm("would you like to add numbers?"); //Boolean popup
-
+   console.log(askLength)
+   if ((askLength < 8) || (asklength > 128 )){
+    writePassword();
+  } else { 
+    askLength = askLength
+   
+  }
 
   let password = generatePassword(askLength,charLower,charUpper,charSpecial,charNumber); //values from inputs
-  let passwordText = document.querySelector("#password");//finds password box in document
-  passwordText.value = password;//Output Password to form
+ let passwordText = document.querySelector("#password");//finds password box in document
+ passwordText.value = password;//Output Password to form
 }
 function generatePassword(askLength,charLower,charUpper,charSpecial,charNumber){ 
-let passString = charSmall
+  let passString =""
+if (askLower) passString = passString.concat(charLower);
 if (askUpper) passString = passString.concat(charUpper);
 if (askSpecial) passString = passString.concat(charSpecial);
 if (askNumber) passString = passString.concat(charNumber);
 let passwordCharacters = ""; //Store characters
+
 // let myArray =passwordCharacters.split("")
-for (let x = 0, x < askLength x++ ){
-  let character =passwordCharacters[Math.floor(Math.random()*askLength)];
-passwordCharacters.push(character)
+for (let x = 0; x < askLength; x++ ){
+  let characterCode = passString [Math.floor(Math.random()*passwordCharacters.length)];//random character
+passwordCharacters.push(string.fromPassString(characterCode))
 }
 console.log(charSmall)
+return passwordCharacters.join('')
 }
 
 
